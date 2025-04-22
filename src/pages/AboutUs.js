e7249da02dde99a04474ecca7a35e01e24aa31c2
Sabin Raut <sabinraut4@gmail.com>
2025-04-22 19:36:20 +1000

    removed helmet from about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,35 +1,16 @@
-// src/pages/AboutUs.js
 import React from 'react';
 import { Element } from 'react-scroll';
-import { Helmet } from 'react-helmet';
+
 import '../styles/style.css';
 
+
 const AboutUs = () => {
+
+  
   return (
     <Element name="about">
       <section className="about-us-section" data-aos="fade-right">
-      <Helmet>
-  <title>About Us | East to West Reblocking</title>
-  <meta name="description" content="East to West Reblocking is Melbourne’s trusted provider for reblocking, restumping, underpinning, and floor levelling. Learn more about our team and values." />
-  <script type="application/ld+json">
-    {JSON.stringify({
-      "@context": "https://schema.org",
-      "@type": "LocalBusiness",
-      "name": "East to West Reblocking Pty Ltd",
-      "url": "https://easttowestreblocking.com.au",
-      "description": "We are Melbourne's trusted experts in reblocking, restumping, underpinning and floor levelling.",
-      "address": {
-        "@type": "PostalAddress",
-        "streetAddress": "7 Wimba Ct",
-        "addressLocality": "Coolaroo",
-        "addressRegion": "VIC",
-        "postalCode": "3048",
-        "addressCountry": "AU"
-      },
-      "telephone": "+61 433 185 183"
-    })}
-  </script>
-</Helmet>
+      
         <div className="about-us-content">
           <h2 data-aos="fade-up" data-aos-delay="100">About Us</h2>
           <p data-aos="fade-up" data-aos-delay="200">
